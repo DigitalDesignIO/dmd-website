2f47c92267b0f50ef521cb08afbb2dd70716aff7
Andi-Lo <contact@andreaslorer.de>
2016-04-21 16:10:07 +0200

    added http to https redirect to htaccess

--- app/site/snippets/footer.php
+++ app/site/snippets/footer.php
@@ -8,7 +8,7 @@
 	    <div class="heading">Social Media</div>
 	    <a href="https://www.facebook.com/digitaldesign.de" target="_blank" class="button facebook icon-facebook"></a>
 	    <br />
-	    <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fdigitaldesign.de&amp;layout=button_count&amp;show_faces=true&amp;action=like&amp;colorscheme=light" style="width:300px;height:20px;" id="lkbtn" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+	    <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fdigitaldesign.de&amp;layout=button_count&amp;show_faces=true&amp;action=like&amp;colorscheme=light" style="width:300px;height:20px;" id="lkbtn" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 	    <br /> 
 	  </div>
 	  <div class="medium-3 columns end">
