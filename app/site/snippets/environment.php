b38a296e532f6dfb4341dba6f26a281634cb5401
Andi-Lo <contact@andreaslorer.de>
2016-10-25 17:30:04 +0200

    fixed indentation

--- app/site/snippets/environment.php
+++ app/site/snippets/environment.php
@@ -1,6 +1,4 @@
-	
 	<!-- environment -->
-
 		<!-- scroll-to anker mark -->
 	  <span data-magellan-destination="<?php echo $data->title()->html() ?>">
 	  	<a name="<?php echo $data->title()->html() ?>"></a>
@@ -8,9 +6,10 @@
 
 		<?php
 
+			// TODO: rework template (hs and environment snippet only differ by their name (mb use data->title()))
 			foreach(page('environment')->children()->visible() as $content):
 				snippet($content->intendedTemplate(), array('data' => $data, 'content' => $content));
 			endforeach
 
 		?>
-	
+	<!-- ./environment -->
