fd4b3e202e6e4e61a3d8e738984d022ab15d1f7d
Andreas Lorer <contact@andreaslorer.de>
2017-04-15 17:41:59 +0200

    fixed pathing for assets and images

--- app/site/tags/arrowlist.php
+++ app/site/tags/arrowlist.php
@@ -1,8 +1,10 @@
 <?php
-
 	kirbytext::$tags['arrow'] = array(
 		'html' => function($tag) {
-			return '<div class="small-2 medium-3 columns icons"><img src="assets/images/arrow.svg" alt="list-arrow"></div><div class="small-10 medium-9 columns text">'. $tag->attr('arrow').'</div>';
+			return
+				'<div class="small-2 medium-3 columns icons">
+				<img src="' . url('assets/images/arrow.svg') . '" alt="list-arrow">
+				</div><div class="small-10 medium-9 columns text">'. $tag->attr('arrow').'</div>';
 		}
 	);
 ?>
--- app/site/tags/image.php
+++ app/site/tags/image.php
@@ -1,18 +1,97 @@
 <?php
+  // image tag extended from the official kirby image tag located under dmd-website/app/kirby/extensions/tags.php#image
+  kirbytext::$tags['image'] = array(
+    'attr' => array(
+      'width',
+      'height',
+      'alt',
+      'text',
+      'title',
+      'class',
+      'imgclass',
+      'linkclass',
+      'caption',
+      'link',
+      'target',
+      'popup',
+      'rel'
+    ),
+    'html' => function($tag) {
 
-	kirbytext::$tags['image'] = array(
-		'html' => function($tag) {
-			return 
-			'<figure>
-				<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" 
-				data-src="'
-					.$tag->page()->uri() . DS 
-					.$tag->attr('image').
-				'" alt="'
-					.$tag->attr('image').
-				'">
-			</figure>';
-			}
-	);
+    $url     = $tag->attr('image');
+    $alt     = $tag->attr('alt');
+    $title   = $tag->attr('title');
+    $link    = $tag->attr('link');
+    $caption = $tag->attr('caption');
+    $file    = $tag->file($url);
+
+    // use the file url if available and otherwise the given url
+    $url = $file ? $file->url() : url($url);
+
+    // alt is just an alternative for text
+    if($text = $tag->attr('text')) $alt = $text;
+
+    // try to get the title from the image object and use it as alt text
+    if($file) {
+
+      if(empty($alt) and $file->alt() != '') {
+        $alt = $file->alt();
+      }
+
+      if(empty($title) and $file->title() != '') {
+        $title = $file->title();
+      }
+
+    }
+
+    // at least some accessibility for the image
+    if(empty($alt)) $alt = ' ';
+
+    // link builder
+    $_link = function($image) use($tag, $url, $link, $file) {
+
+      if(empty($link)) return $image;
+
+      // build the href for the link
+      if($link == 'self') {
+        $href = $url;
+      } else if($file and $link == $file->filename()) {
+        $href = $file->url();
+      } else if($tag->file($link)) {
+        $href = $tag->file($link)->url();
+      } else {
+        $href = $link;
+      }
+
+      return html::a(url($href), $image, array(
+        'rel'    => $tag->attr('rel'),
+        'class'  => $tag->attr('linkclass'),
+        'title'  => $tag->attr('title'),
+        'target' => $tag->target()
+      ));
+
+    };
+
+    // image builder
+    $_image = function($class) use($tag, $url, $alt, $title) {
+      return
+        '<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+          data-src="' . $url . '" alt="' . $alt .'">';
+    };
+
+    if(kirby()->option('kirbytext.image.figure') or !empty($caption)) {
+      $image  = $_link($_image($tag->attr('imgclass')));
+      $figure = new Brick('figure');
+      $figure->addClass($tag->attr('class'));
+      $figure->append($image);
+      if(!empty($caption)) {
+        $figure->append('<figcaption>' . html($caption) . '</figcaption>');
+      }
+      return $figure;
+    } else {
+      $class = trim($tag->attr('class') . ' ' . $tag->attr('imgclass'));
+      return $_link($_image($class));
+    }
+  });
 
 ?>
