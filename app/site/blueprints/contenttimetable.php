422fd9db5374dc27786550d00f40237240ea0b1c
Andi-Lo <contact@andreaslorer.de>
2017-05-30 11:09:42 +0200

    added timetable as a picture

--- app/site/blueprints/contenttimetable.php
+++ app/site/blueprints/contenttimetable.php
@@ -12,126 +12,16 @@ fields:
     label: Flyer PDF download
     help: Für Smartphones wird anstelle des Studienplans eine PDF angeboten
 
-  Semester11:
-    extends: selectcourse
-    label: 1. Semester
-  Semester12: selectcourse
-  Semester13: selectcourse
-  Semester14: selectcourse
-  Semester15: selectcourse
+  imageDesktop:
+    label: Studenplan im Querformat
+    type: select
+    options: images
+    required: true
+    help: Für Desktop und Geräte mit großem Bildschirm.
 
-  Modul11: selectmodule
-  Modul12: selectmodule
-  Modul13: selectmodule
-  Modul14: selectmodule
-  Modul15: selectmodule
-      
-  colour11: selectcolour
-  colour12: selectcolour
-  colour13: selectcolour
-  colour14: selectcolour
-  colour15: selectcolour
-    
-  Semester21:
-    extends: selectcourse
-    label: 2. Semester
-  Semester22: selectcourse
-  Semester23: selectcourse
-  Semester24: selectcourse
-  Semester25: selectcourse
-
-  Modul21: selectmodule
-  Modul22: selectmodule
-  Modul23: selectmodule
-  Modul24: selectmodule
-  Modul25: selectmodule
-      
-  colour21: selectcolour
-  colour22: selectcolour
-  colour23: selectcolour
-  colour24: selectcolour
-  colour25: selectcolour
-
-  Semester31:
-    extends: selectcourse
-    label: 3. Semester
-  Semester32: selectcourse
-  Semester33: selectcourse
-  Semester34: selectcourse 
-  Semester35: selectcourse
-
-  Modul31: selectmodule
-  Modul32: selectmodule
-  Modul33: selectmodule
-  Modul34: selectmodule
-  Modul35: selectmodule
-  
-  colour31: selectcolour
-  colour32: selectcolour
-  colour33: selectcolour
-  colour34: selectcolour
-  colour35: selectcolour
-
-  Semester41:
-    extends: selectcourse
-    label: 4. Semester
-  Semester42: selectcourse
-  Semester43: selectcourse
-  Semester44: selectcourse
-  Semester45: selectcourse
-
-  Modul41: selectmodule
-  Modul42: selectmodule
-  Modul43: selectmodule
-  Modul44: selectmodule
-  Modul45: selectmodule
-
-  colour41: selectcolour
-  colour42: selectcolour
-  colour43: selectcolour
-  colour44: selectcolour
-  colour45: selectcolour
-
-  Semester51: selectcourse
-  Semester52: selectcourse
-  Semester53: selectcourse
-  Semester54: selectcourse
-  Semester55: selectcourse
-  
-  Modul51: selectmodule
-  Modul52: selectmodule
-  Modul53: selectmodule
-  Modul54: selectmodule
-  Modul55: selectmodule
-  
-  colour51: selectcolour
-  colour52: selectcolour
-  colour53: selectcolour
-  colour54: selectcolour
-  colour55: selectcolour
-  
-  Semester61:
-    extends: selectcourse
-    label: 6. Semester
-    width: 5/5
-    
-  colour61:
-    extends: selectcolour
-    width: 1/1
-
-  Semester71:
-    extends: selectcourse
-    label: 7. Semester
-    width: 1/2
-    
-  Semester72:
-    extends: selectcourse
-    width: 1/2
-  
-  colour71:
-    extends: selectcolour
-    width: 1/2
-    
-  colour72:
-    extends: selectcolour
-    width: 1/2
+  imageMobile:
+    label: Studenplan im Hochformat
+    type: select
+    options: images
+    required: true
+    help: Für Smartphones und Geräte mit kleinem Bildschirm.
--- app/site/snippets/contenttimetable.php
+++ app/site/snippets/contenttimetable.php
@@ -1,209 +1,25 @@
- <!-- content-timetable -->
+  <!-- content-timetable -->
     <div class="content-table">
       <div class="row content-table-headlines">
         <div class="medium-12 columns"><h4><?php echo $data->title()->html() ?></h4></div>
         <div class="medium-12 columns"><h5><?php echo $content->headline()->html() ?></h5></div>
       </div>
 
+      <!-- content-image -->
+      <div class="row content">
+        <?php if( $content->imageDesktop()->isNotEmpty() && $content->imageMobile()->isNotEmpty()): ?>
+          <picture>
+            <source srcset="<?php echo $content->imageDesktop()->toFile()->thumb(['width' => 1220, 'quality' => 80])->url(); ?>" media="(min-width: 768px)">
+            <source srcset="<?php echo $content->imageMobile()->toFile()->thumb(['width' => 767, 'quality' => 80])->url(); ?>" media="(min-width: 500px) and (max-width: 767px)">
+            <img srcset="<?php echo $content->imageDesktop()->toFile()->thumb(['width' => 1220, 'quality' => 80])->url(); ?>" alt="Timetable DigitalMediaDesign">
+          </picture>
+        <?php endif ?>
+      </div>
+      <!-- ./content-image -->
+
       <div class="show-for-small-only text-center">
         <?php echo $content->flyer()->kirbytext() ?>
       </div>
 
-      <div class="show-for-medium-up">
-        <!-- table headlines -->
-        <div class="row">
-          <div class="table-headlines">
-            <div class="medium-1 columns"><div>Semester</div></div>
-            <div class="medium-3 columns"><div>10 ECTS</div></div>
-            <div class="medium-2 columns"><div>5 ECTS</div></div>
-            <div class="medium-2 columns"><div>5 ECTS</div></div>
-            <div class="medium-2 columns"><div>5 ECTS</div></div>
-            <div class="medium-2 columns"><div>5 ECTS</div></div>
-          </div>
-        </div>
-
-    
-        <!-- table row Semester 1 -->
-        <div class="row">
-          <div class="medium-1 columns font-xl no-padding-right"><div class="<?php echo $content->colour11()->kirbytextRaw() ?>">1</div></div>
-
-          <div class="medium-3 columns no-padding-left">
-            <div class="<?php echo $content->colour11()->kirbytextRaw() ?>"><?php echo $content->semester11()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul11()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour12()->kirbytextRaw() ?>"><?php echo $content->semester12()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul12()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">  
-            <div class="<?php echo $content->colour13()->kirbytextRaw() ?>"><?php echo $content->semester13()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul13()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">  
-            <div class="<?php echo $content->colour14()->kirbytextRaw() ?>"><?php echo $content->semester14()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul14()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour15()->kirbytextRaw() ?>"><?php echo $content->semester15()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul15()->kirbytextRaw() ?></span></div>
-          </div>
-        </div>
-
-        <!-- table row Semester 2 -->
-        <div class="row">
-          <div class="medium-1 columns font-xl no-padding-right"><div class="<?php echo $content->colour21()->kirbytextRaw() ?>">2</div></div>
-
-          <div class="medium-3 columns no-padding-left">
-            <div class="<?php echo $content->colour21()->kirbytextRaw() ?>"><?php echo $content->semester21()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul21()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour22()->kirbytextRaw() ?>"><?php echo $content->semester22()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul22()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour23()->kirbytextRaw() ?>"><?php echo $content->semester23()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul23()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour24()->kirbytextRaw() ?>"><?php echo $content->semester24()->kirbytextRaw() ?><br /> 
-            <span class="text-muted"><?php echo $content->modul24()->kirbytextRaw() ?></span></div>
-          </div>          
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour25()->kirbytextRaw() ?>"><?php echo $content->semester25()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul25()->kirbytextRaw() ?></span></div>
-          </div>
-        </div>
-
-
-        <!-- table row Semester 3 -->
-        <div class="row">
-          <div class="medium-1 columns font-xl no-padding-right"><div class="<?php echo $content->colour31()->kirbytextRaw() ?>">3</div></div>
-          <div class="medium-3 columns no-padding-left">
-            <div class="<?php echo $content->colour31()->kirbytextRaw() ?>"><?php echo $content->semester31()->kirbytextRaw() ?> <br />
-            <span class="text-muted"><?php echo $content->modul31()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour32()->kirbytextRaw() ?>"><?php echo $content->semester32()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul32()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour33()->kirbytextRaw() ?>"><?php echo $content->semester33()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul33()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour34()->kirbytextRaw() ?>"><?php echo $content->semester34()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul34()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour35()->kirbytextRaw() ?>"><?php echo $content->semester35()->kirbytextRaw() ?><br />
-            <span class="text-muted"><?php echo $content->modul35()->kirbytextRaw() ?></span></div>
-          </div>
-        </div>
-
-        <!-- table row Semester 4-->
-        <div class="row">
-          <div class="medium-1 columns font-xl no-padding-right"><div class="<?php echo $content->colour41()->kirbytextRaw() ?>">4</div></div>
-          <div class="medium-3 columns no-padding-left">
-            <div class="<?php echo $content->colour41()->kirbytextRaw() ?>"><?php echo $content->semester41()->kirbytextRaw() ?> <br />
-            <span class="text-muted"><?php echo $content->modul41()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour42()->kirbytextRaw() ?>"><?php echo $content->semester42()->kirbytextRaw() ?> <br />
-            <span class="text-muted"><?php echo $content->modul42()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour43()->kirbytextRaw() ?>"><?php echo $content->semester43()->kirbytextRaw() ?> <br />
-            <span class="text-muted"><?php echo $content->modul43()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour44()->kirbytextRaw() ?>"><?php echo $content->semester44()->kirbytextRaw() ?> <br />
-            <span class="text-muted"><?php echo $content->modul44()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour45()->kirbytextRaw() ?>"><?php echo $content->semester45()->kirbytextRaw() ?> <br />
-            <span class="text-muted"><?php echo $content->modul45()->kirbytextRaw() ?></span></div>
-          </div>
-        </div>
-
-        <!-- table row Semester 5-->
-        <div class="row">
-          <div class="medium-1 columns font-xl no-padding-right"><div class="<?php echo $content->colour51()->kirbytextRaw() ?>">5</div></div>
-          <div class="medium-3 columns no-padding-left">
-            <div class="border-color-primary"><?php echo $content->semester51()->kirbytextRaw() ?> <br /> 
-            <span class="text-muted"><?php echo $content->modul51()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour52()->kirbytextRaw() ?>"><?php echo $content->semester52()->kirbytextRaw() ?> <br />
-            <span class="text-muted"><?php echo $content->modul52()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour53()->kirbytextRaw() ?>"><?php echo $content->semester53()->kirbytextRaw() ?> <br /> 
-            <span class="text-muted"><?php echo $content->modul53()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour54()->kirbytextRaw() ?>"><?php echo $content->semester54()->kirbytextRaw() ?>  <br> 
-            <span class="text-muted"><?php echo $content->modul54()->kirbytextRaw() ?></span></div>
-          </div>
-          <div class="medium-2 columns">
-            <div class="<?php echo $content->colour55()->kirbytextRaw() ?>"><?php echo $content->semester55()->kirbytextRaw() ?>  <br> 
-            <span class="text-muted"><?php echo $content->modul55()->kirbytextRaw() ?></span></div>
-          </div>
-        </div>
-
-        <!-- table row Semester 6-->
-        <div class="row">
-          <div class="medium-1 columns font-xl no-padding-right"><div class="<?php echo $content->colour61()->kirbytextRaw() ?>">6</div></div>
-          <div class="medium-11 columns no-padding-left">
-            <div class="<?php echo $content->colour61()->kirbytextRaw() ?>"><?php echo $content->semester61()->kirbytextRaw() ?> </div>
-          </div>
-        </div>
-
-        <!-- table row Semester 7-->
-        <div class="row">
-          <div class="medium-1 columns font-xl no-padding-right"><div class="<?php echo $content->colour71()->kirbytextRaw() ?>">7</div></div>
-          <div class="medium-5 columns no-padding-left">
-            <div class="<?php echo $content->colour71()->kirbytextRaw() ?>"><?php echo $content->semester71()->kirbytextRaw() ?> </div>
-          </div>
-          <div class="medium-6 columns">
-            <div class="<?php echo $content->colour72()->kirbytextRaw() ?>"><?php echo $content->semester72()->kirbytextRaw() ?> </div>
-          </div>
-        </div>
-
-      </div>
-      <!-- ./content-table -->
     </div>
-    <!-- ./table-wrapper -->
-
-    <!-- legende -->
-    <div class="row legende show-for-medium-up">
-      <div class="large-2 large-offset-10 columns"><div class="heading">Legende:</div>
-        <div class="row">
-          <div class="small-4 columns end border-color-primary no-padding-lr"></div>
-        </div>
-        <div class="row">
-          <div class="small-12 columns">Gestaltungsfächer</div>
-        </div>
-        <div class="row">
-          <div class="small-4 columns end border-color-black no-padding-lr"></div>
-        </div>
-        <div class="row">
-          <div class="small-12 columns">Große Projekte</div>
-        </div>
-        <div class="row">
-          <div class="small-4 columns end border-color-secondary no-padding-lr"></div>
-        </div>
-        <div class="row">
-          <div class="small-12 columns">Werkzeuge, Programmieren, Mathematik
-          </div>
-        </div>
-        <div class="row">
-          <div class="small-4 columns end border-color-choice choice no-padding-lr"></div>
-        </div>
-        <div class="row">
-          <div class="small-12 columns">Wahlfächer aus Design &amp; Programmieren
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- ./legende -->
+  <!-- content-timetable -->
